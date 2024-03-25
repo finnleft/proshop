47ab519c47483aacce65e3d07a50f6e314eec9e7
BuildTools <unconfigured@null.spigotmc.org>
2024-03-25 15:24:35 -0400

    Pagination components on user-facing and admin product pages

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -1,15 +1,18 @@
 import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button, Row, Col } from "react-bootstrap";
 import { FaEdit, FaTrash } from "react-icons/fa";
+import { useParams } from "react-router-dom";
 import Message from "../../components/Message";
+import Paginate from "../../components/Paginate";
 import Loader from "../../components/Loader";
 import { toast } from "react-toastify";
 import { useGetProductsQuery, useCreateProductMutation, useDeleteProductMutation } from "../../slices/productsApiSlice";
 
 const ProductListScreen = () => {
-  const { data: products, isLoading, error, refetch } = useGetProductsQuery();
+  const { pageNumber } = useParams();
+  const { data, isLoading, error, refetch } = useGetProductsQuery();
 
-  const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation();
+  const [createProduct, { isLoading: loadingCreate }] = useCreateProductMutation({pageNumber});
   
   const createProductHandler = async () => {
     if (window.confirm("Create a new product?")) {
@@ -66,7 +69,7 @@ const ProductListScreen = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        { products.map((product) => (
+                        { data.products.map((product) => (
                             <tr key={product._id}>
                                 <td>{product._id}</td>
                                 <td>{product.name}</td>
@@ -87,6 +90,7 @@ const ProductListScreen = () => {
                         ))}
                     </tbody>
                 </Table>
+                <Paginate pages={data.pages} page={data.page} isAdmin={true}/>
             </>
         )}
     </>
