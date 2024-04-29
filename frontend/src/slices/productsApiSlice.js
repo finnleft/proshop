ee02abe1a2d976dc616419d285d373334ca5c45c
finnleft <117303324+finnleft@users.noreply.github.com>
2024-04-28 22:27:34 -0400

    Revert "Finn"

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -62,14 +62,8 @@ export const productsApiSlice = apiSlice.injectEndpoints({
                 url: `${PRODUCTS_URL}/top`,
             }),
             keepUnusedDataFor: 5,
-        }),
-        shareToSocials: builder.query({
-            query: (id) => ({
-                url: `${PRODUCTS_URL}/${id}/share`
-            }),
-            keepUnusedDataFor: 5,
         })
     }),
 });
 
-export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation, useGetTopProductsQuery, useShareToSocialsQuery } = productsApiSlice;
+export const { useGetProductsQuery, useGetProductDetailsQuery, useCreateProductMutation, useUpdateProductMutation, useUploadProductImageMutation, useDeleteProductMutation, useCreateReviewMutation, useGetTopProductsQuery } = productsApiSlice;
